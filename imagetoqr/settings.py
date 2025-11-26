6b349c5fa47c31931d748e555d4b7f136fbf95be
Aakash <119574470+Aakashsahu4@users.noreply.github.com>
2025-11-26 15:24:40 +0530

    Update settings.py

--- imagetoqr/settings.py
+++ imagetoqr/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+SECRET_KEY = 'django-insecure-b#dssk$pr_=oat6k=7!0h4v5#rqtuf49dj!aco@p0_vs0x&nv^'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
